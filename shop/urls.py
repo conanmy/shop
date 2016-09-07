e1ffff848a00c0041223bc132cc2a53298bb5f93
FrankMa <semstar2010@gmail.com>
2016-09-07 20:15:11 +0800

    auth try login

--- shop/urls.py
+++ shop/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url('^', include('django.contrib.auth.urls')),
     url(r'^product/', include('main.urls')),
     url(r'^admin/', admin.site.urls),
 ]
