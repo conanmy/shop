17d8cb835c160fbf949e08f5259dfca8f1d22523
FrankMa <semstar2010@gmail.com>
2016-09-11 20:14:10 +0800

    add registration

--- shop/urls.py
+++ shop/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url('^', include('django.contrib.auth.urls')),
     url(r'^product/', include('main.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
