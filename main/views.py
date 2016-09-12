7dbbffb8b25fb91a9ebede399024ba7c60d82621
FrankMa <semstar2010@gmail.com>
2016-09-12 20:14:05 +0800

    login redirect url and user access permission

--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-
+from django.contrib.auth.decorators import login_required
 from .models import Product
 
+@login_required()
 def index(request):
     productList = Product.objects.order_by('-id')[:5]
     context = {
@@ -10,6 +11,7 @@ def index(request):
     }
     return render(request, 'list.html', context)
 
+@login_required()
 def detail(request, productId):
     product = get_object_or_404(Product, pk=productId)
     return render(request, 'detail.html', {'product': product})
