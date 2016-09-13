82366f9fbcf0559cc34dc3730b3ffa5f85d5333f
FrankMa <semstar2010@gmail.com>
2016-09-13 20:05:28 +0800

    order and record table

--- main/models.py
+++ main/models.py
@@ -22,4 +22,19 @@ class Product(models.Model):
     productPicture2 = models.CharField(max_length=100)
     productPicture3 = models.CharField(max_length=100)
 
-        
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField()
+    date = models.DateField()
+    buyerName = models.CharField(max_length=100)
+    buyerEmail = models.CharField(max_length=100)
+    buyerPhoneNumber = models.CharField(max_length=100)
+    buyerAddress = models.CharField(max_length=100)
+
+class Record(models.Model):
+    id = models.AutoField(primary_key=True)
+    order_id = models.IntegerField()
+    product_id = models.IntegerField()
+    productName = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    retailPrice = models.BigIntegerField()
