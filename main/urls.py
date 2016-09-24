ec7ec5b75103697137542a84d9b74bea68a2fb5a
FrankMa <semstar2010@gmail.com>
2016-09-24 09:03:19 +0800

    header style changes

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^product/$', views.index, name='index'),
     url(r'^product/(?P<productId>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^cart/', views.cart, name='cart'),
-    url(r'^order/', views.order, name='order'),
-    url(r'^orders/', views.orders, name='orders'),
+    url(r'^cart/$', views.cart, name='cart'),
+    url(r'^order/$', views.order, name='order'),
+    url(r'^orders/$', views.orders, name='orders'),
 ]
